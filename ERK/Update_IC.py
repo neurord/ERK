717639c59fde31df2288797b62c8b637a00c9684
Nadia <nadia@bupkis.krasnow.gmu.edu>
2018-10-24 16:21:51 -0400

    First working version Update_IC

--- ERK/Update_IC.py
+++ ERK/Update_IC.py
@@ -7,14 +7,6 @@ import h5py as h5
 def decode(table):
     return np.array([s.decode('utf-8') for s in table])
 
-def get_mol_index(simData,outputset,molecule):
-    species = decode(simData['model']['output'][outputset]['species'])
-    indices=np.where(species == molecule)[0]
-    if len(indices) == 1:
-        return indices[0]
-    else:
-        return -1
-
 def get_mol_info(simData,plot_molecules,gridpoints):
     outputsets=list(simData['model']['output'].keys())
     dt=np.zeros((len(plot_molecules)))
@@ -47,6 +39,16 @@ def get_mol_info(simData,plot_molecules,gridpoints):
                 print("** Even Worse: MOLECULE",molecule, " DOES NOT EXIST !!!!!!!!!!!!!")
     return out_location,dt,samples
          
+def get_mol_index(simData,outputset,molecule):
+    species = decode(simData['model']['output'][outputset]['species'])
+    indices=np.where(species == molecule)[0]
+    if len(indices) == 1:
+        return indices[0]
+    else:
+        return -1
+
+
+
 #parameters
 input_filename='IC_ERK-Test.xml'
 h5filename='Model_ERK-Test.h5'
@@ -74,20 +76,19 @@ trials=[a for a in data.keys() if 'trial' in a]
 arraysize=len(trials)
 outputsets=data[trials[0]]['output'].keys()
 out_location,dt,rows=get_mol_info(data,molecules,maxvols)
-voxel=0
-
 
 #put data into a dictionary
 molec_conc_ic={}
 for mol in molecules:
-            num_mols=len(molecules)
-            outset =list(out_location[mol]['location'].keys())[0]
-            imol=out_location[mol]['location'][outset]['mol_index']
-            start_time=int(float(time_args.split(" ")[0])//dt[imol])
-            end_time=int(float(time_args.split(" ")[1])//dt[imol])
-            tempConc=np.zeros((len(trials),out_location[mol]['samples']))
-            tempConc=data[trials[0]]['output'][outset]['population'][:,voxel,imol]/TotVol/mol_per_nM_u3
-            molec_conc_ic[mol]=int(np.mean(tempConc[start_time:end_time]))
+    num_mols=len(molecules)
+    outset =list(out_location[mol]['location'].keys())[0]
+    imol=out_location[mol]['location'][outset]['mol_index']
+    voxel=out_location[mol]['location'][outset]['elements']
+    start_time=int(float(time_args.split(" ")[0])//dt[imol])
+    end_time=int(float(time_args.split(" ")[1])//dt[imol])
+    tempConc=np.zeros((len(trials),out_location[mol]['samples']))
+    tempConc=data[trials[0]]['output'][outset]['population'][:,voxel,imol]/TotVol/mol_per_nM_u3
+    molec_conc_ic[mol]=np.round(np.mean(tempConc[start_time:end_time,:]),3)
             
 
 #read in the xml file
@@ -103,13 +104,22 @@ for elem in root:
                                     print('SurfaceDens',subelem.attrib)  
 
 #loop over all molecules in conc_IC dictionary and update values
-for mol,val in molec_conc_ic[mol]:
-            print('dictionary',mol,val)
-            elems=tree.findall('.//NanoMolarity[@specieID="'+mol+'"]')[0]
-            print(mol,elems.attrib,'new value', val)
-            elems.attrib['value']=str(val)
-            print('updated elems',elems.attrib)
-   
+problems_list=[]
+for mol,val in molec_conc_ic.items():
+            elems=tree.findall('.//NanoMolarity[@specieID="'+mol+'"]')
+            if len(elems)==1:
+                print(mol,elems[0].attrib,'new value', val)
+                elems[0].attrib['value']=str(val)
+                print('updated elems',elems[0].attrib)
+            else:
+                problems_list.append((mol,elems))
+if len(problems_list):
+    print('*******************zero or more than 1 species found')
+    for item in problems_list:
+        print(item)
+    
+
+
 #write the new xml file
 with open(outfile, 'wb') as out:
             out.write(ET.tostring(root))
