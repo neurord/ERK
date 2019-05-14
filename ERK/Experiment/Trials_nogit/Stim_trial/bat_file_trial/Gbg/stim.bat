87e440b9e6b9ca7955c953f66d00421815c250b8
Nadia <nadia@bupkis.krasnow.gmu.edu>
2019-05-14 14:19:23 -0400

    cleaning

--- ERK/Experiment/Trials_nogit/Stim_trial/bat_file_trial/Gbg/stim.bat
+++
@@ -1,7 +0,0 @@
-
-
-#stimulate 
-java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 3600000 Model_ERK-stimd.xml
-#java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 3600000 Model_ERK-stimdold.xml
-#java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 3600000 Model_ERK-stimdd.xml
-#java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 3600000 Model_ERK-stimd-nobRafRap1MEK.xml
