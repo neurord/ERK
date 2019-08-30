bc725c4d9a6d8d5ba2fccbf92d13c2599b665181
Nadia <nadia@bupkis.vsnet.gmu.edu>
2019-08-30 16:23:19 -0400

    Organized directories and update files with new model

---
+++ Experiment/simulation/1_Single_path/cAMP/Knockout/args.py
@@ -0,0 +1,22 @@
+ARGS="IC_ERK-Test,Model_ERK-BCK,3200 3600"  
+exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+
+
+ARGS="IC_ERK-Test,Model_ERK-BpSynGap,3200 3600"  
+exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+
+ARGS="IC_ERK-Test,Model_ERK-BSynden,3200 3600"  
+exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+
+
+
+
+
+
+
