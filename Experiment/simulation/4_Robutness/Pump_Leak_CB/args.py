2324a84f982f36e9575ba27737fbca9cda8a4ff9
Nadia <nadia@bupkis.vsnet.gmu.edu>
2019-09-13 11:19:04 -0400

    updated stim files to shape pulses

---
+++ Experiment/simulation/4_Robutness/Pump_Leak_CB/args.py
@@ -0,0 +1,46 @@
+#ARGS="IC_ERK-Test-CB1e6Pump50Leak500,Model_ERK-CB1e6Pump50Leak500,3000 3500"  
+#exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+#ARGS="IC_ERK-Test-CB3e6Pump50Leak500,Model_ERK-CB3e6Pump50Leak500,3000 3500"  
+#exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+
+#ARGS="IC_ERK-Test-CB1e6Pump500Leak800,Model_ERK-CB1e6Pump500Leak800,3000 3500"  
+#exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+
+#ARGS="IC_ERK-Test-CB3e6Pump500Leak800,Model_ERK-CB3e6Pump500Leak800,3000 3500"  
+#exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+#ARGS="IC_ERK-Test-CB1e6Pump500Leak700,Model_ERK-CB1e6Pump500Leak700,3000 3500"  
+#exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+
+#ARGS="IC_ERK-Test-CB3e6Pump500Leak700,Model_ERK-CB3e6Pump500Leak700,3000 3500"  
+#exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+
+ARGS="IC_ERK-Test-CB3e6Pump500Leak750,Model_ERK-CB3e6Pump500Leak750,3000 3500"  
+exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
