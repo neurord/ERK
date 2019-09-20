c3730ebbcceea93de5baaf87c9d5ec8ccfa8253f
Nadia <nadia@bupkis.vsnet.gmu.edu>
2019-09-20 17:57:38 -0400

    update stim files- no buff to bring cAMP to basal (literature sugest 20 sec before basal)

--- Experiment/simulation/1_Single_path/cAMP/Knockout/args.py
+++ Experiment/simulation/1_Single_path/cAMP/Knockout/args.py
@@ -1,22 +1,33 @@
-ARGS="IC_ERK-Test,Model_ERK-BCK,3200 3600"  
+ARGS="IC_ERK-Test,Model_ERK-BEpacAMP,3200 3600"  
 exec(open('/home/nadia/ERK/Update_IC-block.py').read())
 
 
 
 
-ARGS="IC_ERK-Test,Model_ERK-BpSynGap,3200 3600"  
+ARGS="IC_ERK-Test,Model_ERK-BEpacRap,3200 3600"  
 exec(open('/home/nadia/ERK/Update_IC-block.py').read())
 
 
 
-ARGS="IC_ERK-Test,Model_ERK-BSynden,3200 3600"  
+ARGS="IC_ERK-Test,Model_ERK-BPKAC3G,3200 3600"  
+exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+
+
+
+ARGS="IC_ERK-Test,Model_ERK-BPKACbl,3200 3600"  
 exec(open('/home/nadia/ERK/Update_IC-block.py').read())
 
 
 
 
+ARGS="IC_ERK-Test,Model_ERK-BPKARap,3200 3600"  
+exec(open('/home/nadia/ERK/Update_IC-block.py').read())
 
 
+
+
+ARGS="IC_ERK-Test,Model_ERK-BPKASrc,3200 3600"  
+exec(open('/home/nadia/ERK/Update_IC-block.py').read())
 
 
 
