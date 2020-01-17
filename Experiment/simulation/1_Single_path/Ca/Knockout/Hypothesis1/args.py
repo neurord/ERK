9deb0d588c441b59de593b66d66b64268773d2df
Nadia <nminingo@gmu.edu>
2020-01-17 10:49:53 -0500

    update stim files

--- Experiment/simulation/1_Single_path/Ca/Knockout/Hypothesis1/args.py
+++ Experiment/simulation/1_Single_path/Ca/Knockout/Hypothesis1/args.py
@@ -1,21 +1,21 @@
 ARGS="Ca/Knockout/Hypothesis1/IC_ERK-Test,Ca/Knockout/Hypothesis1/Model_ERK-BCK-control,3000 3600"  
-exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+exec(open('/home/nadia/ERK/Update_IC.py').read())
 
 
 ARGS="Ca/Knockout/Hypothesis1/IC_ERK-Test,Ca/Knockout/Hypothesis1/Model_ERK-BCKauto-control,3000 3600"  
-exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+exec(open('/home/nadia/ERK/Update_IC.py').read())
 
 
 ARGS="Ca/Knockout/Hypothesis1/IC_ERK-Test,Ca/Knockout/Hypothesis1/Model_ERK-BpSynGap-control,3000 3600"  
-exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+exec(open('/home/nadia/ERK/Update_IC.py').read())
 
 
 ARGS="Ca/Knockout/Hypothesis1/IC_ERK-Test,Ca/Knockout/Hypothesis1/Model_ERK-BSynden-control,3000 3600"  
-exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+exec(open('/home/nadia/ERK/Update_IC.py').read())
 
 
 ARGS="Ca/Knockout/Hypothesis1/IC_ERK-Test,Ca/Knockout/Hypothesis1/Model_ERK-BpSynGapKcat-control,3000 3600"  
-exec(open('/home/nadia/ERK/Update_IC-block.py').read())
+exec(open('/home/nadia/ERK/Update_IC.py').read())
 
 
 
