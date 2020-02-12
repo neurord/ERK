78492df22ac3f17222c53e47fe3c9e9fed26df61
Nadia <nminingo@gmu.edu>
2020-02-12 09:39:25 -0500

    update

--- Experiment/simulation/4_Robustness/args.py
+++ Experiment/simulation/4_Robustness/args.py
@@ -1,28 +1,28 @@
-ARGS="old-CamCa/IC_ERK-Testold,old-CamCa/Model_ERK-oldCam,3000 3600"
+#ARGS="old-CamCa/IC_ERK-Testold,old-CamCa/Model_ERK-oldCam,3000 3600"
+#exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
+
+
+ARGS="pp1_ck-ratio/IC_ERK-Test-R05,pp1_ck-ratio/Model_ERK-R05,3000 3600"
 exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
 
-
-ARGS="pp1_ck-ratio/IC_ERK-Test-R.5,pp1_ck-ratio/Model_ERK-R05,3000 3600"
+ARGS="pp1_ck-ratio/IC_ERK-Test-R07,pp1_ck-ratio/Model_ERK-R07,3000 3600"
 exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
 
-ARGS="pp1_ck-ratio/IC_ERK-Test-R.7,pp1_ck-ratio/Model_ERK-R07,3000 3600"
-exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
-
-ARGS="pp1_ck-ratio/IC_ERK-Test-R.18,pp1_ck-ratio/Model_ERK-R018,3000 3600"
+ARGS="pp1_ck-ratio/IC_ERK-Test-R018,pp1_ck-ratio/Model_ERK-R018,3000 3600"
 exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
 
 ARGS="pp1_ck-ratio/IC_ERK-Test-R1,pp1_ck-ratio/Model_ERK-R1,3000 3600"
 exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
 
 
-ARGS="NewPKA/IC_ERK-Test,NewPKA/Model_ERK-newPKA,3000 3600"
-exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
+#ARGS="NewPKA/IC_ERK-Test,NewPKA/Model_ERK-newPKA,3000 3600"
+#exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
 
 
-ARGS="SRC-quantity/IC_ERK-Test-src40,SRC-quantity/Model_ERK-src40,3000 3600"
-exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
+#ARGS="SRC-quantity/IC_ERK-Test-src40,SRC-quantity/Model_ERK-src40,3000 3600"
+#exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
 
-ARGS="SRC-quantity/IC_ERK-Test-src80,SRC-quantity/Model_ERK-src80,3000 3600"
-exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
+#ARGS="SRC-quantity/IC_ERK-Test-src80,SRC-quantity/Model_ERK-src80,3000 3600"
+#exec(open('/home/nadia/ERK/UpdateIC_basal.py').read())
 
 
